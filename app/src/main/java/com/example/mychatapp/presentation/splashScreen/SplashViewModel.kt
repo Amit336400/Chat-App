ad23afa524b3c00e280c79d7510904b8c1cfa046
Amit <amitkundu113920@gmail.com>
2024-12-10 08:47:56 +0530

    fixes #25 : Useless TaskState

--- app/src/main/java/com/example/mychatapp/presentation/splashScreen/SplashViewModel.kt
+++ app/src/main/java/com/example/mychatapp/presentation/splashScreen/SplashViewModel.kt
@@ -5,8 +5,6 @@ import com.example.mychatapp.domain.local.repo.PreferenceRepo
 import com.example.mychatapp.presentation.navigation.Routes
 import com.streamliners.base.BaseViewModel
 import com.streamliners.base.ext.execute
-import com.streamliners.base.taskState.load
-import com.streamliners.base.taskState.taskStateOf
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -16,23 +14,20 @@ class SplashViewModel @Inject constructor(
     private val repo: PreferenceRepo,
 ) : BaseViewModel() {
 
-    val taskState = taskStateOf<Unit>()
 
     fun checkUserIsLoginOrNot(
         navHostController: NavHostController,
     ) {
         execute(showLoadingDialog = false) {
-            taskState.load {
 
-                val isLoggedIn = repo.getLoginState()
+            val isLoggedIn = repo.getLoginState()
 
-                withContext(Dispatchers.Main) {
-                    if (isLoggedIn){
-                        navHostController.navigate(Routes.HomeScreen)
-                    }
-                    else{
-                        navHostController.navigate(Routes.LoginScreen)
-                    }
+            withContext(Dispatchers.Main) {
+                if (isLoggedIn){
+                    navHostController.navigate(Routes.HomeScreen)
+                }
+                else{
+                    navHostController.navigate(Routes.LoginScreen)
                 }
             }
         }
